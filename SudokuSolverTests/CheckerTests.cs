ded6476a02ae4ca7940c6903d48a91d214f4cd5f
Joachim Grubbe Frank <joac1067@stud.kea.dk>
2021-03-20 17:22:41 +0100

    Add project files.

---
+++ SudokuSolverTests/CheckerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SudokuSolverTests
+{
+    [TestClass]
+    public class CheckerTests
+    {
+        [TestMethod]
+        public void IsBoxValid_PartialFilled_True()
+        {
+            //Arrange
+             //Need 2 dimensional array
+             //fill with number a few places
+
+            //Act
+            //Run method that chekcs
+
+            //assert
+            //is true returned??
+        }
+
+    }
+}
---
+++ TDDSudokuSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDDSudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

53c3e1cda5f8122383c9b04d9c2605ff2f58e3d8
Joachim Grubbe Frank <joac1067@stud.kea.dk>
2021-03-21 13:53:36 +0100

    Added tests for Checker class to be implemented Added checker class and method IsArrayValid()

--- SudokuSolverTests/CheckerTests.cs
+++ SudokuSolverTests/CheckerTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDSudokuSolver;
 
 namespace SudokuSolverTests
 {
@@ -11,21 +12,85 @@ namespace SudokuSolverTests
         [TestMethod]
         public void IsArrayValid_PartialFilled_True()
         {
+            //A 0 in the array counts as an empty space, therefore it is called "partialFilled."
             //Arrange
-            //Need 2 dimensional array
-            //fill with number a few places
-            int[] Array = new int[9];
+            int[] Array = new int[9] {1,0,4,8,2,0,9,0,0 };
             bool ExpectedResult = true;
 
             //Act
-            //Run method that chekcs
-            bool ActualResult = CheckerTests.IsArrayValid(Array);
+            bool ActualResult = Checker.IsArrayValid(Array);
 
 
             //assert
-            //is true returned??
             Assert.AreEqual(ExpectedResult, ActualResult);
         }
 
+
+        [TestMethod]
+        public void IsArrayValid_PartialFilled_False()
+        {
+            //A 0 in the array counts as an empty space, therefore it is called "partialFilled."
+            //Arrange
+            int[] Array = new int[9] { 1, 0, 4, 8, 2, 0, 9, 1, 1 };
+            bool ExpectedResult = false;
+
+            //Act
+            bool ActualResult = Checker.IsArrayValid(Array);
+
+
+            //assert
+            Assert.AreEqual(ExpectedResult, ActualResult);
+        }
+
+
+        [TestMethod]
+        public void IsArrayValid_Filled_True()
+        {
+            //A 0 in the array counts as an empty space, therefore it is called "partialFilled."
+            //Arrange
+            int[] Array = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            bool ExpectedResult = true;
+
+            //Act
+            bool ActualResult = Checker.IsArrayValid(Array);
+
+
+            //assert
+            Assert.AreEqual(ExpectedResult, ActualResult);
+        }
+
+
+        [TestMethod]
+        public void IsArrayValid_Filled_false()
+        {
+            //A 0 in the array counts as an empty space, therefore it is called "partialFilled."
+            //Arrange
+            int[] Array = new int[9] { 1, 1, 3, 4, 5, 6, 7, 8, 9 };
+            bool ExpectedResult = false;
+
+            //Act
+            bool ActualResult = Checker.IsArrayValid(Array);
+
+
+            //assert
+            Assert.AreEqual(ExpectedResult, ActualResult);
+        }
+
+
+        [TestMethod]
+        public void IsArrayValid_Empty_True()
+        {
+            //A 0 in the array counts as an empty space, therefore it is called "partialFilled."
+            //Arrange
+            int[] Array = new int[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            bool ExpectedResult = true;
+
+            //Act
+            bool ActualResult = Checker.IsArrayValid(Array);
+
+
+            //assert
+            Assert.AreEqual(ExpectedResult, ActualResult);
+        }
     }
 }
---
+++ TDDSudokuSolver/Checker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDSudokuSolver
+{
+    public static class Checker
+    {
+        public static bool IsArrayValid(int[] array)
+        {
+            List<int> CheckList = new List<int>();
+            foreach(int X in array)
+            {
+                if(X != 0)
+                {
+                    bool contains = CheckList.Contains(X);
+                    
+                    switch (contains)
+                    {
+                        case true:
+                            return false;
+                        case false:
+                            CheckList.Add(X);
+                            break;
+                    }
+                        
+                    
+                }
+
+            }
+            return true;
+        }
+    }
+}

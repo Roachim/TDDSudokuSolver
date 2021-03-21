f9aeceff51402ab5f6ca55aeb51d893a72fc62df
Joachim Grubbe Frank <joac1067@stud.kea.dk>
2021-03-21 13:14:28 +0100

    started building test cases

--- SudokuSolverTests/CheckerTests.cs
+++ SudokuSolverTests/CheckerTests.cs
@@ -9,17 +9,22 @@ namespace SudokuSolverTests
     public class CheckerTests
     {
         [TestMethod]
-        public void IsBoxValid_PartialFilled_True()
+        public void IsArrayValid_PartialFilled_True()
         {
             //Arrange
-             //Need 2 dimensional array
-             //fill with number a few places
+            //Need 2 dimensional array
+            //fill with number a few places
+            int[] Array = new int[9];
+            bool ExpectedResult = true;
 
             //Act
             //Run method that chekcs
+            bool ActualResult = CheckerTests.IsArrayValid(Array);
+
 
             //assert
             //is true returned??
+            Assert.AreEqual(ExpectedResult, ActualResult);
         }
 
     }
